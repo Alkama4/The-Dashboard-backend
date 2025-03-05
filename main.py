bf2e2c5ad9865036c1e6f540e9e8e04b1daddaf3
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-03-05 13:56:49 +0200

    Store resized images

--- main.py
+++ main.py
@@ -3192,31 +3192,25 @@ def get_title_info(
 
 
 @app.get("/image/{image_path:path}")
-@app.head("/image/{image_path:path}")
 async def get_image(image_path: str, width: int = Query(None)):
-    full_path = os.path.join("/fastapi-images", image_path)
+    base_path = "/fastapi-images"
+    full_path = os.path.join(base_path, image_path)
 
     if not os.path.exists(full_path):
         raise HTTPException(status_code=404, detail="Image doesn't exist.")
+    
+    if width and width in [300, 600, 900, 1200]:
+        resized_path = f"{os.path.splitext(full_path)[0]}_{width}{os.path.splitext(full_path)[1]}"
+        if not os.path.exists(resized_path):
+            img = Image.open(full_path)
+            aspect_ratio = img.height / img.width
+            new_height = int(width * aspect_ratio)
+            img = img.resize((width, new_height))
+            img.save(resized_path)
 
-    if "head" in str(get_image.__name__).lower():
-        return {"message": "Image exists"}
-
-    if width:   
-        img = Image.open(full_path)
-        aspect_ratio = img.height / img.width
-        new_height = int(width * aspect_ratio)
-        img = img.resize((width, new_height))
-
-        img_io = BytesIO()
-        img_format = img.format or "PNG"
-        img.save(img_io, format=img_format)
-        img_io.seek(0)
-
-        return StreamingResponse(img_io, media_type=f"image/{img_format.lower()}")
+        return FileResponse(resized_path)
 
     return FileResponse(full_path)
-
 
 
 # Sort by options for future "/watch_list/list_titles":
